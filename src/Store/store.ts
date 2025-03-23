8fe4d5f3d55f61583a8541a024fd623718027c7f
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-03-23 15:21:45 +0530

    feat: zustand store updated

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,25 +1,69 @@
 import { create } from "zustand";
 
+{/*Grid State */}
 type Gridtype = {
-    gridsize: number;
-    setGridsize: (size: number)
+    gridSize: number;
+    setgridSize: (size: number) => void
 }
 export const useGridsize = create<Gridtype>((set) => ({
-    gridsize: 2,
-    setgridSize: (size) => set({gridzize: size})
+    gridSize: 4,
+    setgridSize: (size: number) => set({gridSize: size})
 }))
 
+{/*Card State */}
+type Card = {
+        id: number;
+        num: number;
+      }
 
+type Cardtype = {
+    cards: Card[];
+    setCards: (size: Card[]) => void;
+}
 
+export const useCards = create<Cardtype>((set) => ({
+    cards: [],
+    setCards: (newCards) => set({cards: newCards})
+}))
 
+{/*Cardflipped State */}
+type Flippedtype = {
+    flipped: number[];
+    setFlipped: (newFlip: []) => unknown
+}
+export const useFlipped = create<Flippedtype>((set) => ({
+    flipped: [],
+    setFlipped: (newFlip) => set({flipped: newFlip})
+}))
 
+{/*Solved State */}
+type Solvedtype = {
+    solved: number[];
+    setSolved: (solvedArr: []) => unknown;
+}
 
+export const useSolved = create<Solvedtype>((set) => ({
+    solved: [],
+    setSolved: (solvedArr) => set({solved: solvedArr})
+}))
 
-
-
+{/*Won State */}
 type Wontype = {
     won: boolean;
+    setWon: (status: boolean) => void;
 }
 export const useWon = create<Wontype>((set) => ({
     won: false,
+    setWon: (status) => set({won: status})
+}))
+
+{/*Disabled State */}
+type Disabledtype = {
+    disabled: boolean;
+    setDisabled: (state: boolean) => void;
+}
+
+export const useDisabled = create<Disabledtype>((set) => ({
+    disabled: false,
+    setDisabled: (state: boolean) => set({disabled: state})
 }))
