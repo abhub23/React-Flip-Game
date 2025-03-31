334fef1ae09e1b8a52ec5c548b124f591466e400
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-01 00:53:28 +0530

    feat:: footer added

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div className="fixed bottom-0 h-10 flex justify-center items-center">
+            Designed and Created by <a className="ml-2 text-blue-400 hover:text-blue-600 hover:underline" target="_blank" 
+            href="https://x.com/abdullah_twt23">Abdullah</a>
+        </div>
+    )
+}
+
+export default Footer;
