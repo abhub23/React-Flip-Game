d4d0798d0680b4ec3f8bbda04593401d125133cf
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-04-03 00:13:30 +0530

    fix: zustand hook ready to change theme from local storage

--- src/Store/useDarkmode.ts
+++ src/Store/useDarkmode.ts
@@ -1,17 +1,17 @@
 import { create } from "zustand";
 
 type Darkmodetype = {
-    isDarkmode: "dark" | "light";
-    toggleDarkmode: () => void
-}
+  isDarkmode: "dark" | "light";
+  toggleDarkmode: () => void;
+};
 
 export const useDarkmode = create<Darkmodetype>((set) => ({
-    
-    isDarkmode: (localStorage.getItem("theme") as "dark" | "light") || "light",
-   
-    toggleDarkmode: () => set((theme) => {
-        const toggle = theme.isDarkmode == "dark" ? "light" : "dark"
-        localStorage.setItem("theme" , toggle)
-        return{isDarkmode: toggle}
-    }) 
-}))
+  isDarkmode: (localStorage.getItem("theme") as "dark" | "light") || "dark",
+
+  toggleDarkmode: () =>
+    set((theme) => {
+      const toggle = theme.isDarkmode == "dark" ? "light" : "dark";
+      localStorage.setItem("theme", toggle);
+      return { isDarkmode: toggle };
+    }),
+}));
