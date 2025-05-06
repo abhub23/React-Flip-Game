3b28688a9f2e114654e6b3c2bdde4321c27a06d7
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-06 23:59:58 +0530

    ui: some ui changes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <div className='fixed bottom-0 h-10 flex flex-col justify-center items-center text-black dark:text-white'>
       <p className='flex flex-row items-center mx-2'>
-      <a href="https://x.com/abdullah_twt23" target='_blank' className='mr-2 hover:text-blue-600'>Twitter</a> <DotFilledIcon/>
-      <a href="https://github.com/abhub23" target='_blank' className='mx-2 hover:text-blue-600'>Github</a> <DotFilledIcon /> 
-      <a href="https://www.linkedin.com/in/abdullah-mukri-84a56b220/" target='_blank' className='ml-2 hover:text-blue-600'>LinkedIN</a>  </p>
+      <a href="https://x.com/abdullah_twt23" target='_blank' className='mr-2 transition-all duration-300 hover:text-blue-500'>Twitter</a> <DotFilledIcon/>
+      <a href="https://github.com/abhub23" target='_blank' className='mx-2 transition-all duration-300 hover:text-blue-500'>Github</a> <DotFilledIcon /> 
+      <a href="https://www.linkedin.com/in/abdullah-mukri-84a56b220/" target='_blank' className='ml-2 transition-all duration-300 hover:text-blue-500'>LinkedIN</a>  </p>
 
       <div className='mb-4 flex flex-row items-center'>
        <PiCopyrightBold/> <span className='mr-2'>2025</span> Designed and Created by{" "}
         <a
-          className="ml-2 text-blue-400 hover:text-blue-600 hover:underline"
+          className="ml-2 text-blue-400 hover:underline"
           target="_blank"
           href="https://x.com/abdullah_twt23"
         >
