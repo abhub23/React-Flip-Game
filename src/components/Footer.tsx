7345a5d396ae311538be029e5dfceab13454c4c0
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-13 23:30:17 +0530

    feat: dynamic date added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
 import { DotFilledIcon } from '@radix-ui/react-icons'
+import { useEffect, useState } from 'react';
 import { PiCopyrightBold } from "react-icons/pi";
 
 const Footer = () => {
+  const [year, setYear] = useState<number | null>(null)
+
+  useEffect(() => {
+    const date = new Date()
+    setYear(date.getFullYear())
+  },[year])
 
   return (
     <div className='fixed bottom-0 h-10 flex flex-col justify-center items-center text-black dark:text-white'>
@@ -11,7 +18,7 @@ const Footer = () => {
       <a href="https://www.linkedin.com/in/abdullah-mukri-84a56b220/" target='_blank' className='ml-2 transition-all duration-300 hover:text-blue-500'>LinkedIN</a>  </p>
 
       <div className='mb-4 flex flex-row items-center'>
-       <PiCopyrightBold/> <span className='mr-2'>2025</span> Designed and Created by{" "}
+       <PiCopyrightBold/> <span className='mr-2'>{year}</span> Designed and Created by{" "}
         <a
           className="ml-2 text-blue-400 hover:underline"
           target="_blank"
