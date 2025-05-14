437d51964b90174d607b47be409663e7e43e9c59
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-14 23:54:01 +0530

    feat: provider added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "next-themes";
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
+
+createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
-    
-)
+);
