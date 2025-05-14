8f526116847f7d6eb63fdb1fee79c0baa21654a3
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-14 23:57:46 +0530

    ui: motion effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { Routes, Route } from "react-router-dom"
-import LandingPage from "./pages/LandingPage"
-import Game from "./pages/Game"
-import ErrorPage from "./pages/ErrorPage"
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Game from "./pages/Game";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/Game" element={<Game />} />
-      <Route path="*" element={<ErrorPage/>} />
+      <Route path="/game" element={<Game />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
-  )
-
+  );
 }
 
-export default App
+export default App;
