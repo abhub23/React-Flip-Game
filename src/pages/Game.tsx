577a75f3d00481f0f769641490979c601c454b28
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-14 21:56:21 +0530

    feat: game logic added

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,83 +1,164 @@
-import { useEffect, useState } from 'react'
-import '../index.css'
-
+import { useEffect } from "react";
+import {
+  useCards,
+  useDisabled,
+  useFlipped,
+  useGridsize,
+  useSolved,
+  useWon,
+} from "../store/store";
+import {
+  SunIcon,
+  MoonIcon,
+} from '@radix-ui/react-icons';
+import Footer from "../components/Footer";
+import { useTheme } from "next-themes";
+import { motion } from "motion/react";
 const Game = () => {
-    type Card = {
-        id: number;
-        num: number;
-      }
-    
-      const [gridSize, setgridSize] = useState(4)
-      const [cards, setCards] = useState<Card[]>([])
-      const [flipped, setFlipped] = useState([])
-      const [solved, setSolved] = useState([])
-      const [won, setWon] = useState(false)
-      const [disabled, setDisabled] = useState(false)
-    
-      const handleGridSize = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const size = parseInt(e.target.value)
-        if (size >= 2 && size <= 10) {
-          setgridSize(size)
-        }
-      }
-    
-      const initializeGame = () => {
-        const totalCards = gridSize * gridSize; //Initially 16
-        const pairsCount = Math.floor(totalCards / 2) //Math.floor for numbers like 5*5 will be 25/2, so 12.5 will be 12
-        const numberArr = [...Array(pairsCount).keys()].map(n => n + 1) //keys() is necessary to expand the iterator returned by .keys() into individual values so that they can be logged as an array of numbers.
-        const shuffelCards: Card[] = [...numberArr, ...numberArr]
-          .sort(() => Math.random() - 0.7)
-          .map((num, idx) => ({ id: idx, num }))
-    
-        setCards(shuffelCards)
-        //all states will get reset if a new game starts...
+
+  const { theme, setTheme } = useTheme()
+
+  const handleTheme = () => {
+    setTheme(theme == 'light' ? 'dark' : 'light')
+  }
+  type Card = {
+    id: number;
+    num: number;
+  };
+  const { gridSize, handleGridSize } = useGridsize();
+  const { cards, setCards } = useCards();
+  const { flipped, setFlipped } = useFlipped();
+  const { solved, setSolved } = useSolved();
+  const { won, setWon } = useWon();
+  const { disabled, setDisabled } = useDisabled()
+
+  const initializeGame = () => {
+    const totalCards = gridSize * gridSize; //Initially 16
+    const pairsCount = Math.floor(totalCards / 2); //Math.floor for numbers like 5*5 will be 25/2, so 12.5 will be 12
+    const numberArr = [...Array(pairsCount).keys()].map((n) => n + 1); //keys() is necessary to expand the iterator returned by .keys() into individual values so that they can be logged as an array of numbers.
+    const shuffelCards: Card[] = [...numberArr, ...numberArr]
+      .sort(() => Math.random() - 0.7)
+      .map((num, idx) => ({ id: idx, num }));
+
+    setCards(shuffelCards);
+    //all states will get reset if a new game starts...
+    setFlipped([]);
+    setSolved([]);
+    setWon(false);
+  };
+
+  useEffect(() => {
+    initializeGame();
+  }, [gridSize]);
+
+  useEffect(() => {
+    if (solved.length === cards.length) {
+      setWon(true)
+    }
+  }, [solved])
+
+  const Solved = (secondId: number) => {
+    const firstId = flipped[0]  // getting first id from flipped array
+    if (cards[firstId].num === cards[secondId].num) {
+      setSolved([...solved, firstId, secondId])
+      setFlipped([])
+      setDisabled(false)
+    } else {
+      setTimeout(() => {
         setFlipped([])
-        setSolved([])
-        setWon(false)
+        setDisabled(false)
+      }, 1000);
+    }
+  }
+
+  const handleClick = (id: number) => {
+    if (disabled || won) {
+      return
+    }
+
+    if (flipped.length === 0) {
+      setFlipped([id]);
+      return;
+    }
+
+    if (flipped.length === 1) {
+      setDisabled(true)
+      if (id != flipped[0]) {
+        setFlipped([...flipped, id])
+        Solved(id)
+      } else {
+        setDisabled(false)
       }
-    
-      useEffect(() => {
-        initializeGame()
-      }, [gridSize])
-    
-      return (
-        <>
-          <div className='flex flex-col min-h-screen bg-red-50 items-center justify-center p-4'>
-            <h1 className='mb-6 text-xl'>MEMORY GAME</h1>
-            <div className='font-thin'>
-              <label htmlFor="gridSize" className='mr-2'>Select Size: (Max 10) </label>
-              <input type="number"
-                id='gridSize'
-                min="2"
-                max={10}
-                value={gridSize}
-                className='px-2 py-1 border border-1 border-black rounded'
-                onChange={handleGridSize}
-              />
-            </div>
-    
-            {/*Game Board */}
-            
-            <div className={'grid gap-4 m-4'}
+    }
+  }
+
+  const isFlipped = (id: number) => flipped.includes(id) || solved.includes(id)
+  const isSolved = (id: number) => solved.includes(id)
+
+  return (
+    <>
+      <div>
+        <div className={`flex flex-col min-h-screen bg-red-50 dark:bg-zinc-950 items-center justify-center p-4 font-mono`}>
+          <div className="fixed mb-2 flex justify-center items-end">
+            <h1 className='fixed top-6 mb-2 text-2xl text-black dark:text-white'>MEMORY GAME</h1>
+            <p className="fixed right-10 top-6 h-[24px] dark:text-white mb-2" onClick={handleTheme}>{theme == 'light' ? <MoonIcon className="h-[20px] w-[30px] cursor-pointer" /> : <SunIcon className="h-[20px] w-[30px] cursor-pointer" />}</p>
+          </div>
+
+          <div className="fixed top-[60px]  font-thin flex items-center justify-center">
+            <label htmlFor="gridSize" className='mr-2 text-xl text-black dark:text-white'>
+              Select Size: ({gridSize} x {gridSize}){" "}
+            </label>
+            <input
+              type="number"
+              id="gridSize"
+              value={gridSize}
+              className='px-2 py-1 border-2 w-20 dark:text-white rounded border-black dark:border-zinc-700'
+              onChange={handleGridSize}
+              step={2}
+              min={2}
+              max={10}
+            />
+          </div>
+
+          {/*Game Board */}
+
+          <motion.div
+       
+      initial={{ opacity: 0 , y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+            className={"grid gap-4  lg:mt-[60px] "}
             style={{
               gridTemplateColumns: `repeat(${gridSize}, minmax(0,1fr))`,
-              width: `min(100, ${gridSize} * 60)rem`
-            }}>
-              {cards.map(card => {
-                return <div 
-                className='aspect-square flex justify-center items-center bg-slate-400 text-xl font-bold cursor-pointer 
-                rounded-sm transition-all duration-300' 
-                key={card.id}>
-                  {card.num}
+              width: `min(100, ${gridSize} * 60)rem`,
+            }}
+          >
+            {cards.map((card) => {
+              return (
+                <div
+                  className={`aspect-square h-[30px] flex justify-center shadow-lg shadow-rose-300 border-black 
+              border-[1px] items-center text-black  text-md font-thin cursor-pointer 
+              rounded-sm transition-all duration-300 font-mono ${isFlipped(card.id) ? isSolved(card.id) ? 'bg-green-300' : 'bg-blue-300' : 'bg-slate-200'}`}
+                  key={card.id}
+                  onClick={() => {
+                    handleClick(card.id)
+                  }}
+                >
+                  {isFlipped(card.id) ? card.num : '?'}
                 </div>
-              })}
-            </div>
-          </div>
-    
-    
-    
-        </>
-      )
-}
+              );
+            })}
+          </motion.div>
+          {won && <div className="lg:p-[2px] lg:h-[40px] lg:mt-[24px] lg:text-[32px] font-semibold text-green-500 animate-bounce"> You won </div>}
+          {<button className="lg:p-[4px] lg:px-[18px] lg:h-[40px] lg:mt-[28px] lg:mb-[14px] lg-h-[80px] text-white bg-black dark:text-black dark:bg-white  border-1 border-black rounded-[6px] dark:border-white lg:text-[22px] font-semibold cursor-pointer"
+            onClick={initializeGame}>{won ? 'Play again' : 'Reset'}
+          </button>}
+          <Footer />
+        </div>
+      </div>
 
-export default Game
+    </>
+  );
+};
+
+export default Game;
