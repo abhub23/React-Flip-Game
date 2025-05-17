98465a8abc30005da58182b9b84e2ba73a8042b1
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-05-17 21:48:30 +0530

    fix: some ui changes and mobile res changes

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,18 +4,18 @@ const ErrorPage: React.FC = () => {
   const navigate = useNavigate();
 
   const handleButton = () => {
-    navigate("/");
+    navigate("/game");
   };
   return (
-    <div className="bg-white flex flex-col justify-center items-center h-screen">
-      <div className="font-mono font-bold text-6xl mb-2">Error 404</div>
-      <div className="font-mono font-bold text-4xl mb-5">Page Not Found</div>
-      <div className="font-mono text-bold text-xl mb-5">
+    <div className="bg-white dark:bg-zinc-950 flex flex-col justify-center items-center h-screen">
+      <div className="font-mono font-bold lg:text-6xl text-[36px] mb-2 text-black dark:text-white">Error 404</div>
+      <div className="font-mono font-bold lg:text-4xl text-[20px] mb-5 text-black dark:text-white">Page Not Found</div>
+      <div className="font-mono text-bold lg:text-xl text-[14px] mb-5 text-black dark:text-white mx-[22px] text-center">
         It seems like the page you're looking for isn't available
       </div>
       <button
         onClick={handleButton}
-        className="h-12 w-40 p-2 font-mono text-md rounded-3xl border-4 border-black hover:bg-black hover:text-white hover:border-zinc-400 transition duration-500"
+        className="lg:h-10 h-[30px] lg:p-[6px] p-[3px] font-mono lg:text-[18px] text-[12px] rounded-[6px] border-[2px] text-white dark:text-black border-black bg-black dark:bg-white transition duration-500 cursor-pointer"
       >
         Back to Game
       </button>
